3fd2d84d5742b7a2ba35e8e2be2cc991730c7a60
kemal1132 <kemalworks1132@gmail.com>
2021-03-24 15:48:03 +0300

    Added jsDoc for language reducer

--- src/Redux/Reducers&Selectors/LanguageReducer.js
+++ src/Redux/Reducers&Selectors/LanguageReducer.js
@@ -1,7 +1,12 @@
 import ActionTypes from "../../Constants/ActionTypes";
 import InitialState from "../../Constants/InitialState";
 
-
+/** Redux Reducer for language variable
+ * @param {Object} state A state parameter that includes language:{string}
+ * @param {string} action takes changeLanguage Action from Constants/ActionTypes 
+ * @returns {Object} new state
+ *
+ */
 
 const language = (state=InitialState.language, action) => {
     if (action.type === ActionTypes.changeLanguage) {
