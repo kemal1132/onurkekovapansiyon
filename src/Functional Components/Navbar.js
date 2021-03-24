019c54380ae319d58863a8fc6dcec2b190230ca3
kemal1132 <kemalworks1132@gmail.com>
2021-03-24 15:42:51 +0300

    Added a placeholder icon

--- src/Functional Components/Navbar.js
+++ src/Functional Components/Navbar.js
@@ -1,6 +1,11 @@
 import {useSelector} from "react-redux";
 import LanguageSelector from "../Redux/Reducers&Selectors/LanguageSelector";
-
-const Navbar = ()=>(<div>{`${useSelector(LanguageSelector)}`}</div>);
+import icon from "../Assets/Icon.png";
+const Navbar = ()=>(
+    <div id="navBar">
+        <img src={icon} alt="OnurPension" id="icon"></img>
+        {`${useSelector(LanguageSelector)}`}
+    </div>
+);
 
 export default Navbar;
