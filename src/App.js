4b94dc267dd3f57d987e813a7a0851736a8ad9df
kemal1132 <kemalworks1132@gmail.com>
2021-03-25 16:38:33 +0300

    Removed some code from createreactapp

--- src/App.js
+++ src/App.js
@@ -8,15 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CollapsingNavBar>This is where my navbar would be...If I had one<NavBar/></CollapsingNavBar>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CollapsingNavBar><NavBar/></CollapsingNavBar>
+       
       </div>
     );
   }
